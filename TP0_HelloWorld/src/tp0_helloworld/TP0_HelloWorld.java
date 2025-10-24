bddd5c7306a2512f0a61a25cf712c7fd26036e76
Arthus <Arthus.bailly@gmail.com>
2025-10-24 10:43:29 +0200

    Création d'un nouveau projet versionné

---
+++ TP0_HelloWorld/src/tp0_helloworld/TP0_HelloWorld.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp0_helloworld;
+
+import java.util.Scanner;
+
+/**
+ *
+ * Arthus Bailly
+ */
+public class TP0_HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+String prenom;
+Scanner sc ;
+sc = new Scanner (System.in) ;
+System.out.println("Quel est votre prénom ?");
+prenom = sc.nextLine();
+    }
+    
+}
