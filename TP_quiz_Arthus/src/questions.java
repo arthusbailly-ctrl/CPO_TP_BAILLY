3b93dce474d2ed09cd07d2734c46b2adcb29a9fb
Arthus <Arthus.bailly@gmail.com>
2025-11-26 18:22:14 +0100

    TP4 finie

---
+++ TP_quiz_Arthus/src/questions.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ME
+ */
+public class questions {
+
+    private String intitule;
+    private String proposition1;
+    private String proposition2;
+    private String proposition3;
+    private String proposition4;
+    private int indexBonneReponse; // valeur entre 1 et 4
+
+    // Constructeur qui initialise tous les attributs
+    public questions(String intitule, String p1, String p2, String p3, String p4, int indexBonneReponse) {
+        this.intitule = intitule;
+        this.proposition1 = p1;
+        this.proposition2 = p2;
+        this.proposition3 = p3;
+        this.proposition4 = p4;
+
+        if (indexBonneReponse < 1 || indexBonneReponse > 4) {
+            throw new IllegalArgumentException("L'index de la bonne réponse doit être entre 1 et 4.");
+        }
+
+        this.indexBonneReponse = indexBonneReponse;
+    }
+
+
+    public String getIntitule() {
+        return intitule;
+    }
+
+    public String getProposition1() {
+        return proposition1;
+    }
+
+    public String getProposition2() {
+        return proposition2;
+    }
+
+    public String getProposition3() {
+        return proposition3;
+    }
+
+    public String getProposition4() {
+        return proposition4;
+    }
+
+    public int getIndexBonneReponse() {
+        return indexBonneReponse;
+    }
+}
+
